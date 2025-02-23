dab6c2fbf37bdf356f29e1b870f55d59753729a4
LeeSiu <167082095+leesiuuuu@users.noreply.github.com>
2025-02-23 15:14:41 +0900

    Ranking Field Added
    랭킹 필드 추가

---
+++ Assets/Scenes/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+	public GameObject Car;
+	
+	public void GoRanking()
+	{
+		StartCoroutine(move(-1,0.5f));
+	}
+
+	public void GoMain()
+	{
+		StartCoroutine(move(1, 0.5f));
+	}
+
+	private IEnumerator move(int d, float duration)
+	{
+		Vector3 curPos = Car.transform.position;
+
+		float ElapsedTime = 0f;
+		while (ElapsedTime < duration)
+		{
+			ElapsedTime += Time.deltaTime;
+			float t = ElapsedTime / duration;
+			Car.transform.position = Vector3.Lerp(curPos, curPos + new Vector3(d, 0, 0), t);
+			yield return null;
+		}
+	}
+}
